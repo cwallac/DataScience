ff8cb8346117177f7c40e800272c1f82fdb02349
brendan <brendan.ritter@yahoo.com>
2015-02-15 22:40:32 -0500

    Added two more charts. One is a map of crime locations in a google map.

--- crime_analysis.py
+++ crime_analysis.py
@@ -3,6 +3,8 @@ import thinkplot
 import thinkstats2
 import collections
 import numpy
+from ast import literal_eval as make_tuple
+import pygmaps
 import matplotlib.pyplot as plt
 from datetime import datetime,date
 
@@ -36,7 +38,6 @@ def plotCategorical(hist):
 	plt.xticks(range(len(sort_hist)),list(sort_hist.keys()),rotation="vertical")
 	plt.show()
 
-
 def createCrimeType(df):
 	# nature=thinkstats2.Hist(df["INCIDENT_TYPE_DESCRIPTION"])
 	# plotCategorical(nature)
@@ -53,6 +54,46 @@ def createCrimeType(df):
 	plt.title("Number of types of crime in Boston")
 	plt.savefig("Plots/CrimeTypes.png",format="png", dpi=72)
 
+def createDailyCrime(df):
+	count=collections.Counter(df["DAY_WEEK"])
+	sort_count=collections.OrderedDict(sorted(count.items(),key=lambda t:t[1],reverse=True))
+	plt.bar(range(len(sort_count)),sort_count.values())
+	plt.xticks(range(len(sort_count)),list(sort_count.keys()),rotation="vertical")
+	plt.subplots_adjust(bottom=0.2)
+	plt.xlabel("Day of Week")
+	plt.ylabel("Number of Crimes")
+	plt.title("Crime per Day")
+	plt.savefig("Plots/DailyCrime.png",format="png", dpi=72)
+
+def createGeograph(df):
+
+	mymap = pygmaps.maps(42.3, -71.1, 16)
+
+	locs=df["Location"]
+	a=0
+	for el in locs:
+		if a%20==0:
+			(lat,lon)=make_tuple(el)
+			mymap.addpoint(lat, lon, "#0000FF")
+		a+=1
+
+	mymap.draw('Plots/mymap.html')
+
+
+
+
+
+	# fname = 'boston.png'
+	# image = Image.open(fname).convert("L")
+	# arr = np.asarray(image)
+	# plt.imshow(arr, cmap = cm.Greys_r)
+	# plt.show()
+	# thinkplot.Scatter(xs,ys)
+	# thinkplot.Show()
+
+
 df=pandas.DataFrame.from_csv("Data/Crime_Incident_Reports.csv")
 # createMonthlyCrime(df)
-createCrimeType(df)
+# createCrimeType(df)
+# createDailyCrime(df)
+createGeograph(df)
